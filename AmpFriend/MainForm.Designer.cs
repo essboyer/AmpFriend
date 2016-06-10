6918e3deefd8cc28600125d7910e75cd6872746b
essboyer <sean.boyer@marinerinnovations.com>
2016-06-10 16:20:11 -0300

    Updated icon

--- AmpFriend/MainForm.Designer.cs
+++ AmpFriend/MainForm.Designer.cs
@@ -36,6 +36,7 @@ namespace AmpFriend
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.label1 = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
@@ -261,6 +262,7 @@ namespace AmpFriend
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.textBox1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.MaximizeBox = false;
 			this.Name = "MainForm";
 			this.Text = "AmpFriend";
